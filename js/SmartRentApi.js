081fc29e970f055cd975523e92b6b8ab23154bcd
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-03-16 07:00:04 -0700

    added getDevices()

--- js/SmartRentApi.js
+++ js/SmartRentApi.js
@@ -7,6 +7,39 @@ export const SmartRentAPI = {
 
     session(){
         return {user_id: 1111, first_name: 'Jaded', access_token: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+    },
+
+    getDevices(toggled){
+
+        if (toggled){
+            return [
+                {
+                    type: "lock",
+                    is_locked: false,
+                    name: "Front Door"
+                },
+                {
+                    type: 'binary_switch',
+                    is_on: false,
+                    name: 'Plug'
+                }
+            ];  
+
+        } else {
+            return [
+                {
+                    type: "lock",
+                    is_locked: true,
+                    name: "Front Door"
+                },
+                {
+                    type: 'binary_switch',
+                    is_on: true,
+                    name: 'Plug'
+                }
+            ];    
+        }
+
     }
 }
 
