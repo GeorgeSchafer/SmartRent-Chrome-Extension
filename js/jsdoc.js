49d05aefb5cab158cf31445477502ca6464c8f54
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-03-17 17:19:44 -0700

    removed dsstore

---
+++ js/jsdoc.js
@@ -0,0 +1,28 @@
+
+/**
+ * @module script
+ */
+
+/**
+ * @description Script.js performs stores elements, listeners and functions.
+ * This is the description of the script.js file. */
+let script = '';
+
+/** @summary code file that calls other *.js files. */
+function ear() {}
+
+/** @todo Finish the HTML document */
+const url = {
+    index: './index.html',
+    devices: './devices.html',
+    options: './options.html'
+};
+
+/** @todo fill the Elements object */
+const Elements = {};
+
+/** @todo fill the Listeners object */
+const Listeners = {};
+
+/** @todo fill the Functions object */
+const Functions = {};
